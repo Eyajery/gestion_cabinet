2d7300352e8faf525f5608405fc2b87e91fdecde
Eyajery <108415168+Eyajery@users.noreply.github.com>
2023-05-02 22:58:17 +0100

    commit

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -10,6 +10,7 @@ import { User } from 'src/app/models/user';
 })
 export class RegisterComponent implements OnInit {
     user: User = new User();
+    passwordError: string = '';
   constructor(private _cabinetService: UserService,private _router :Router,private _activatedRoute:ActivatedRoute){}
   ngOnInit(): void {
     const isIdPresent =this._activatedRoute.snapshot.paramMap.has('id');
@@ -20,15 +21,45 @@ export class RegisterComponent implements OnInit {
       )
     }
   }
-  saveUser(){
-    this._cabinetService.saveUser(this.user).subscribe(data=>{
-      console.log('response',data);
-      this._router.navigateByUrl("/user");
-    })
+  saveUser() {
+    let passwordError = "";
   
+    if (!this.user.username) {
+      passwordError = "Le nom d'utilisateur est obligatoire.";
+    } else if (!this.user.email) {
+      passwordError = "L'adresse email est obligatoire.";
+    } else if (!this.user.password) {
+      passwordError = "Le mot de passe est obligatoire.";
+    } else if (!this.user.password2) {
+      passwordError = "La confirmation de mot de passe est obligatoire.";
+    }
+  
+    if (!passwordError && !this.user.email.includes('@')) {
+      passwordError = "L'adresse email est invalide.";
+    } else {
+      this._cabinetService.getUsers().subscribe(users => {
+        if (users.some(user => user.email === this.user.email)) {
+          passwordError = "Cet e-mail est déjà enregistré.";
+        } else if (this.user.password !== this.user.password2) {
+          passwordError = "Les mots de passe ne correspondent pas.";
+        }
+  
+        if (passwordError) {
+          this.passwordError = passwordError;
+          return;
+        }
+  
+        this._cabinetService.saveUser(this.user).subscribe(data => {
+          console.log("response", data);
+          this._router.navigateByUrl("/user");
+        });
+      });
+    }
+  }
+  
+ 
   }
 
  
   
   
-}
